055212701bf05558a8692684856f6d13e800604d
Eugen Eisler <eoeisler@gmail.com>
2017-07-03 11:32:59 +0200

    Working JWT AUTH

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,9 +4,10 @@ import {AppComponent} from "./app.component";
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
 import {AuthGuard} from "./auth.guard";
+import {HomeComponent} from "./home/home.component";
 
 const appRoutes: Routes = [
-  {path: '', component: AppComponent, canActivate: [AuthGuard]},
+  {path: '', component: HomeComponent, canActivate: [AuthGuard]},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
 
